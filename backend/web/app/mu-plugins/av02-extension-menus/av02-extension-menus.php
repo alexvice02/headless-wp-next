5e2cfe25f1c4166aaebe6eea2ac1474c90fc5894
alexvice02 <41265365+alexvice02@users.noreply.github.com>
2025-08-08 12:27:46 +0000

    Add headless WordPress theme and API extensions
    - Introduce "WP Headless" theme with custom support for menus.
    - Add a plugin to extend WordPress REST API with menu support.
    - Update Nginx config to rewrite `/api` requests to WordPress REST API.
    - Adjust `.gitignore` for new mu-plugins folder structure.

---
+++ backend/web/app/mu-plugins/av02-extension-menus/av02-extension-menus.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Plugin Name: WP Headless - REST API Menus
+ * Plugin URI: https://github.com/alexvice02/headless-wp-next/tree/main/backend/web/app/mu-plugins/av02-integration-menus
+ * Description: Extends WordPress REST API to support menus
+ * Version: 0.1.0
+ * Author: alexvice02
+ * Author URI: https://github.com/alexvice02
+ * License: GPLv2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Text Domain: wph
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+
+function av02_integration_menus_init() {
+    require_once __DIR__ . '/inc/menus.php';
+}
+
+av02_integration_menus_init();
---
+++ backend/web/app/mu-plugins/av02-extensions.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: WP Headless – API extensions
+Description: Extends WordPress REST API to support default WP functionality and custom plugins
+Version: 0.1.0
+Author: alexvice02
+*/
+
+require_once __DIR__ . '/av02-extension-menus/av02-extension-menus.php';
