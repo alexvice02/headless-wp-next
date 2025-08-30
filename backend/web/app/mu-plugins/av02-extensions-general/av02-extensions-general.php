774843534ba375949eb9d9114185f2023b2a322a
alexvice02 <41265365+alexvice02@users.noreply.github.com>
2025-08-30 19:18:43 +0000

    Add favicon support to site settings REST endpoint

--- backend/web/app/mu-plugins/av02-extensions-general/av02-extensions-general.php
+++ backend/web/app/mu-plugins/av02-extensions-general/av02-extensions-general.php
@@ -4,12 +4,16 @@ add_action('rest_api_init', function () {
     register_rest_route('av02/v1', '/site-settings', [
         'methods' => 'GET',
         'callback' => function () {
+            $favicon_id = get_option('site_icon');
+            $favicon_url = $favicon_id ? wp_get_attachment_url($favicon_id) : null;
+
             return [
                 'name'        => get_bloginfo('name'),
                 'description' => get_bloginfo('description'),
                 'url'         => get_bloginfo('url'),
                 'language'    => get_bloginfo('language'),
-                'custom_logo' => wp_get_attachment_url(get_theme_mod('custom_logo'))
+                'custom_logo' => wp_get_attachment_url(get_theme_mod('custom_logo')),
+                'favicon'     => $favicon_url,
             ];
         },
         'permission_callback' => '__return_true',
