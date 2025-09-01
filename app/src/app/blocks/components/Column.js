6cea5f55a420301ae3c5bb0ef7dce7bf4a40e32c
alexvice02 <41265365+alexvice02@users.noreply.github.com>
2025-09-01 18:13:25 +0000

    Enhance `Column` to support background color based on `attrs` prop.

--- app/src/app/blocks/components/Column.js
+++ app/src/app/blocks/components/Column.js
@@ -1,9 +1,10 @@
 import BlockRenderer from "@/app/blocks/BlockRenderer";
 
-export default function Column({ attrs = {}, children = [] }) {
-  return (
-    <div style={{ flexGrow: 1 }}>
-      <BlockRenderer blocks={children} />
-    </div>
-  );
+export default function Column({attrs = {}, children = []}) {
+
+    return (
+        <div style={{flexGrow: 1, backgroundColor: attrs?.backgroundColor ? `var(--color-${attrs?.backgroundColor})` : 'transparent'}}>
+            <BlockRenderer blocks={children}/>
+        </div>
+    );
 }
