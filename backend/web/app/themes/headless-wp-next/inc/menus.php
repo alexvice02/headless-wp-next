64f9e61db4c6711740142f878f0c57bcf32e574c
alexvice02 <41265365+alexvice02@users.noreply.github.com>
2025-09-07 19:35:33 +0300

    Update menu registration to use dynamic locations from API settings

--- backend/web/app/themes/headless-wp-next/inc/menus.php
+++ backend/web/app/themes/headless-wp-next/inc/menus.php
@@ -5,7 +5,16 @@
  * Register the menus
  *
 */
-register_nav_menus( array(
-    'header_menu' => 'Header menu',
-    'footer_menu' => 'Footer menu'
-) );
+function hwn_register_menus () {
+    $menus_api_enabled = \AV02\Settings\Av02Settings::get_option('api_menu_enabled');
+
+    if( !$menus_api_enabled )
+        return;
+
+    $locations = \AV02\Settings\Av02Settings::get_option('api_menu_locations', []);
+
+    if ($locations) {
+        register_nav_menus(array_combine($locations, $locations));
+    }
+}
+add_action( 'after_setup_theme', 'hwn_register_menus' );
